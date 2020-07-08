a91b8b9ec0c037255880987c9fedfd8046106a23
caorong <cr19961123@icloud.com>
2020-07-08 21:19:49 +0800

    v1

---
+++ phoenix-core/src/main/java/org/apache/T.java
@@ -0,0 +1,12 @@
+package org.apache;
+
+import org.apache.phoenix.schema.types.PRowts;
+
+public class T {
+    public static void main(String[] args) {
+//        PRowts pRowts = PRowts.INSTANCE;
+//        System.out.println(pRowts.getSqlType());
+//        System.out.println(pRowts.getSqlTypeName());
+        System.out.println("ROW_TS".equals("ROW_TS"));
+    }
+}
